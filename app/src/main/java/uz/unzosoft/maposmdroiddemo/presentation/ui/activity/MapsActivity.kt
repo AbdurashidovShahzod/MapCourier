cbaf8dfbde028d11d4d87846d28ed4cc8b60f58e
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-12-14 13:08:13 +0500

    Abdurashidov Shahzod😁 14.12.2021 13:09 ic_courier.xml MapsActivity.kt misc.xml

--- app/src/main/java/uz/unzosoft/maposmdroiddemo/presentation/ui/activity/MapsActivity.kt
+++ app/src/main/java/uz/unzosoft/maposmdroiddemo/presentation/ui/activity/MapsActivity.kt
@@ -8,10 +8,21 @@ import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.compass.CompassOverlay
 import uz.unzosoft.maposmdroiddemo.databinding.ActivityMapsBinding
 import uz.unzosoft.maposmdroiddemo.presentation.ui.base.BaseActivity
+import org.osmdroid.views.overlay.Overlay
+import org.osmdroid.views.overlay.OverlayItem
+import org.osmdroid.views.overlay.ItemizedIconOverlay
+
+import android.R
+
+import android.graphics.drawable.Drawable
+import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener
+
 
 class MapsActivity : BaseActivity<ActivityMapsBinding>() {
 
     private lateinit var map: org.osmdroid.views.MapView
+    var currentLocationOverlay: ItemizedIconOverlay<OverlayItem>? = null
+
 
     override fun bindingActivity(): ActivityMapsBinding {
         return ActivityMapsBinding.inflate(layoutInflater)
@@ -33,6 +44,44 @@ class MapsActivity : BaseActivity<ActivityMapsBinding>() {
         marker.position = geoPoint
         marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER)
         map.overlays.add(marker)
+
+
+        val currentLocation = GeoPoint(41.341010692133864, 69.28675052447048)
+        val currentLocation2 = GeoPoint(41.342785252244624, 69.28698977855444)
+        var myLocationOverlayItem = OverlayItem("Shahzod", "Current Position", currentLocation)
+        var myCurrentLocationMarker = this.resources.getDrawable(R.drawable.sym_action_call)
+        myLocationOverlayItem.setMarker(myCurrentLocationMarker)
+
+        val items = ArrayList<OverlayItem>()
+        items.add(myLocationOverlayItem)
+
+
+
+        myLocationOverlayItem = OverlayItem("Shohruh", "Current Position", currentLocation2)
+        myCurrentLocationMarker = resources.getDrawable(R.drawable.sym_action_call)
+        myLocationOverlayItem.setMarker(myCurrentLocationMarker)
+
+
+        items.add(myLocationOverlayItem)
+
+
+
+        currentLocationOverlay = ItemizedIconOverlay<OverlayItem>(
+            items,object:OnItemGestureListener<OverlayItem>{
+                override fun onItemSingleTapUp(index: Int, item: OverlayItem?): Boolean {
+                    return true
+                }
+
+                override fun onItemLongPress(index: Int, item: OverlayItem?): Boolean {
+                    return true
+                }
+
+            },this
+        )
+        map.overlays.add(this.currentLocationOverlay)
+
+
+
         mapController.animateTo(geoPoint)
         mapController.setCenter(geoPoint)
     }
