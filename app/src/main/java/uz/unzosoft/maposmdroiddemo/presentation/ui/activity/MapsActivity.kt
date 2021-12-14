cc7b25b33467522ec41cc9e9deccb853c6303aa0
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-12-14 12:30:45 +0500

    Abdurashidov Shahzod😁 14.12.2021 12:31 MapsActivity.kt themes.xml themes.xml

--- app/src/main/java/uz/unzosoft/maposmdroiddemo/presentation/ui/activity/MapsActivity.kt
+++ app/src/main/java/uz/unzosoft/maposmdroiddemo/presentation/ui/activity/MapsActivity.kt
@@ -1,27 +1,39 @@
 package uz.unzosoft.maposmdroiddemo.presentation.ui.activity
 
 import android.os.Bundle
+import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.compass.CompassOverlay
 import uz.unzosoft.maposmdroiddemo.databinding.ActivityMapsBinding
 import uz.unzosoft.maposmdroiddemo.presentation.ui.base.BaseActivity
 
 class MapsActivity : BaseActivity<ActivityMapsBinding>() {
 
-    lateinit var map: org.osmdroid.views.MapView
+    private lateinit var map: org.osmdroid.views.MapView
 
     override fun bindingActivity(): ActivityMapsBinding {
         return ActivityMapsBinding.inflate(layoutInflater)
     }
 
     override fun onCreated(savedInstanceState: Bundle?) {
-        map = org.osmdroid.views.MapView(this)
+        map = binding.map
+        Configuration.getInstance().load(this, getSharedPreferences("couirer", MODE_PRIVATE))
+        val mapController = map.controller
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setBuiltInZoomControls(true)
         map.setMultiTouchControls(true)
-        val mapController = map.controller
-        mapController.setZoom(9.5)
+        mapController.setZoom(19.4 )
+        val compassOverlay = CompassOverlay(this,map)
+        compassOverlay.enableCompass()
+        map.overlays.add(compassOverlay)
         val geoPoint = GeoPoint(41.341010692133864, 69.28675052447048)
+        val marker = Marker(map)
+        marker.position = geoPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER)
+        map.overlays.add(marker)
+        mapController.animateTo(geoPoint)
         mapController.setCenter(geoPoint)
     }
 
